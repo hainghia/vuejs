584caf42c2469bbf5f3e78718f7425c7ece17e68
Hai Nghia <nghiahh.nta@gmail.com>
2022-12-13 16:41:59 +0700

    Get dynamic data from backend API (#6)
    Signed-off-by: Hai Nghia <nghiahh.nta@gmail.com>

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,64 +1,70 @@
 <template>
   <AddTodo @add-todo="addTodo"/>
-  <TodoItem
-      v-for="todo in todos"
-      :key="todo.id"
-      :todoProps="todo"
-      @item-completed="markCompleted"
-      @item-deleted="deleted"
-  />
+  <TodoItem v-for="todo in todos" :key="todo.id" :todoProps="todo" @item-completed="markCompleted" @item-deleted="deleted"/>
 </template>
 
 <script>
 
+import axios from 'axios'
 import {ref} from "@vue/reactivity";
 import TodoItem from "@/components/TodoItem.vue";
 import AddTodo from "@/components/AddTodo.vue";
-import {v4 as uuidv4} from 'uuid';
 
 export default {
   name: "Todos",
   components: {AddTodo, TodoItem},
   setup() {
-    const todos = ref([
-      {
-        id: uuidv4(),
-        title: 'Job 1',
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: 'Job 2',
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: 'Job 3',
-        completed: false
-      },
-    ])
+    const todos = ref([])
+    const getAllTodos = async () => {
+      try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        todos.value = res.data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getAllTodos()
     const markCompleted = (id) => {
       // console.log('Parent = ', id)
       todos.value.map(todo => {
         if (todo.id === id) todo.completed = !todo.completed
       })
     }
-    const deleted = (id) => {
-      todos.value = todos.value.filter(
-          todo => {
-            return todo.id !== id
-          }
-      )
+
+    /**
+     *
+     * @param id
+     */
+    const deleted = async (id) => {
+      try {
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        todos.value = todos.value.filter(
+            todo => todo.id !== id
+        )
+      } catch (error) {
+        console.log(error)
+      }
+
+
     }
-    const addTodo = (newTodo) => {
-      todos.value.push(newTodo)
-      console.log(newTodo.id)
+
+    /**
+     *
+     * @param newTodo
+     */
+    const addTodo = async (newTodo) => {
+      try {
+        const res = await axios.post(`https://jsonplaceholder.typicode.com/todos`, newTodo)
+        todos.value.push(res.data)
+      } catch (error) {
+        console.log(error)
+      }
     }
     return {
       todos,
       markCompleted,
       deleted,
-      addTodo
+      addTodo,
     }
   },
 }
